2fbf78e929c8c34f54a70eeb72054b73cf6a28d7
Cody Swithenbank <cody.swithenbank@gmail.com>
2023-01-16 13:07:00 -0500

    build: initial setup

---
+++ src/components/atoms/testComp/TestComp.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./TestComp.css";
+
+export interface TestCompProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+const TestComp = ({
+  primary = true,
+  backgroundColor,
+  size = "medium",
+  onClick,
+  label,
+}: TestCompProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={backgroundColor ? { backgroundColor } : {}}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default TestComp;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import TestComp from "./components/atoms/testComp/TestComp";
+
+export { TestComp };
